8efb30e7b6939b7f38cc644fdf1240ea27e06797
Bereket18 <bereketadamsseged@gmail.com>
2023-09-29 20:21:03 +0300

    setting-screen-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			maxWidth: {
+				container: "1440px",
+			},
+			screens: {
+				xs: "320px",
+				sm: "375px",
+				sml: "500px",
+				md: "667",
+				mdl: "768px",
+				lg: "960px",
+				lgl: "1024px",
+				xl: "1280px",
+			},
+		},
 	},
 	plugins: [],
 };
