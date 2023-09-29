70b1cdae84a464928bd2cd760ad8c6596c0eaebe
Bereket18 <bereketadamsseged@gmail.com>
2023-09-29 21:01:49 +0300

    Export-Import-Assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,29 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className="w-full bg-amazon_blue text-white px-4 py-3">
+			{/*=============Image Start here==============*/}
+
+			{/*=============Image Ends here==============*/}
+			{/*=============Delivery Start here==============*/}
+
+			{/*=============Delivery Ends here==============*/}
+			{/*=============Search Start here==============*/}
+
+			{/*=============Search Ends here==============*/}
+			{/*=============Sign-in Start here==============*/}
+
+			{/*=============Sign-in Ends here==============*/}
+			{/*=============Orders Start here==============*/}
+
+			{/*=============Orders Ends here==============*/}
+			{/*=============Delivery Start here==============*/}
+
+			{/*=============Delivery Ends here==============*/}
+		</div>
+	);
 };
 
 export default Header;
