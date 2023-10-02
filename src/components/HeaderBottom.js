ff5c4bee304143a24756dba8ab110398c4f48878
Bereket18 <bereketadamsseged@gmail.com>
2023-10-02 18:55:29 +0300

    state-sidebar

--- src/components/HeaderBottom.js
+++ src/components/HeaderBottom.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 const HeaderBottom = () => {
+	const [Sidebar, setSidebar] = useState(false);
 	return (
 		<div className="w-full px-4 h-[36px] bg-amazon_light text-white flex items-center">
 			{/* ****************** listItems Start here **************** */}
 			<ul className="flex items-start gap-2 text-sm tracking-tight">
-				<li className="headerHover flex items-center gap-1">
+				<li
+					onClick={() => setSidebar(true)}
+					className="headerHover flex items-center gap-1">
 					<MenuIcon />
 					All
 				</li>
@@ -20,6 +23,7 @@ const HeaderBottom = () => {
 			</ul>
 			{/* ****************** listItems End here  ***************** */}
 			{/* ****************** SlideNav Start here ***************** */}
+
 			{/* ****************** SlideNav Start here ***************** */}
 		</div>
 	);
