0b6e74d119caa1e092179b2ecbf194cfbbf70682
Bereket18 <bereketadamsseged@gmail.com>
2023-10-02 18:43:31 +0300

    bottom-list created

--- src/components/HeaderBottom.js
+++ src/components/HeaderBottom.js
@@ -2,7 +2,20 @@ import React from "react";
 
 const HeaderBottom = () => {
 	return (
-		<div className="w-full px-4 h-[36px] bg-amazon_light text-white flex items-center"></div>
+		<div className="w-full px-4 h-[36px] bg-amazon_light text-white flex items-center">
+			{/* ****************** listItems Start here **************** */}
+			<ul className="flex items-start gap-2 text-sm tracking-tight">
+				<li className="headerHover">All</li>
+				<li className="headerHover">Today's Deals</li>
+				<li className="headerHover"> Customer Service</li>
+				<li className="headerHover">Gift Cards</li>
+				<li className="headerHover">Registry</li>
+				<li className="headerHover">Sell</li>
+			</ul>
+			{/* ****************** listItems End here  ***************** */}
+			{/* ****************** SlideNav Start here ***************** */}
+			{/* ****************** SlideNav Start here ***************** */}
+		</div>
 	);
 };
 
