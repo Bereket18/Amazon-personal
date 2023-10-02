a370a3e81fd720c670cd6b47ddeeb7d07f4656f7
Bereket18 <bereketadamsseged@gmail.com>
2023-10-02 21:34:47 +0300

    Banner component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 import Header from "./components/Header";
+import Banner from "./components/Home/Banner";
 
 function App() {
 	return (
 		<div className="font-bodyFont">
 			<Header />
+			<Banner />
 		</div>
 	);
 }
