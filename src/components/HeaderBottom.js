75832e5b4b04bafef192d89e27b7138bf94059dc
Bereket18 <bereketadamsseged@gmail.com>
2023-10-02 19:38:49 +0300

    sidebar-lists created

--- src/components/HeaderBottom.js
+++ src/components/HeaderBottom.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 const HeaderBottom = () => {
 	const [Sidebar, setSidebar] = useState(false);
@@ -37,6 +38,85 @@ const HeaderBottom = () => {
 								<h3 className="text-lg font-titleFont font-semibold mb-1 px-6">
 									Digital Content & Devices
 								</h3>
+								<ul>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Amazon Music{" "}
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Kindle E-readers & Books{" "}
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Amazon Appstore
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<h3 className="text-lg font-titleFont font-semibold mb-1 px-6">
+										Shop By Department
+									</h3>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Electronics
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Computers
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Smart Home
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+
+									<h3 className="text-lg font-titleFont font-semibold mb-1 px-6">
+										Programs & Features
+									</h3>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Gift Cards
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Amazon live
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										International Shopping
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+
+									<h3 className="text-lg font-titleFont font-semibold mb-1 px-6">
+										Help & Settings
+									</h3>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Your Account
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+									<li className="flex items-center justify-between hover:bg-zinc-200 px-6 py-2 cursor-pointer">
+										Customer Service
+										<span>
+											<KeyboardArrowRightIcon />
+										</span>
+									</li>
+								</ul>
 							</div>
 						</div>
 					</div>
