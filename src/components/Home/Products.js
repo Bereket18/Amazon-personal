bf5be2fb2f197ecfcef9be26b84c69cd656db06f
Bereket18 <bereketadamsseged@gmail.com>
2023-10-04 22:17:51 +0300

    last-fix

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -1,6 +1,8 @@
 // import axios from "axios";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+import StarIcon from "@mui/icons-material/Star";
 
 const Products = () => {
 	const data = useLoaderData();
@@ -16,8 +18,11 @@ const Products = () => {
 						<img
 							className="w-52 h-64 object-contain"
 							src={item.image}
-							alt="product1"
+							alt="product_img"
 						/>
+						<span className="text-xs capitalize italic absolute top-2 right-2">
+							{item.category}
+						</span>
 					</div>
 					<div className="px-4">
 						<div className="flex items-center justify-between">
@@ -29,8 +34,21 @@ const Products = () => {
 							</p>
 						</div>
 						<div>
-							<p>{item.description.substring(0, 100)}...</p>
+							<p className="text-sm ">
+								{item.description.substring(0, 100)}...
+							</p>
+							<div className="text-yellow-500">
+								<StarIcon />
+								<StarIcon />
+								<StarIcon />
+								<StarIcon />
+							</div>
 						</div>
+						<button
+							className="w-full font-titleFont font-medium text-base bg-gradient-to-tr
+            from-yellow-400 t0-yellow-200 border hover:from-yellow-300 hover:to-yellow-400 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3">
+							Add to Cart
+						</button>
 					</div>
 				</div>
 			))}
