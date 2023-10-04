24fa399b7f7cb21dd9f107b9951d00e5005f5cb3
Bereket18 <bereketadamsseged@gmail.com>
2023-10-04 09:12:51 +0300

    Creating-Footer-Folder-and-file

---
+++ src/components/Footer/FooterMiddle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterMiddle = () => {
+	return <div>FooterMiddle</div>;
+};
+
+export default FooterMiddle;
---
+++ src/components/Footer/FooterTop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FooterTop = () => {
+	return <div>FooterTop</div>;
+};
+
+export default FooterTop;
