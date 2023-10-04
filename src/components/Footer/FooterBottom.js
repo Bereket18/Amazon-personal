f696dcbff5329ee93aee12f47004ce66b2a198d0
Bereket18 <bereketadamsseged@gmail.com>
2023-10-04 13:50:14 +0300

    Bottom-lists added to constants

--- src/components/Footer/FooterBottom.js
+++ src/components/Footer/FooterBottom.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const FooterBottom = () => {
-	return <div>FooterBottom</div>;
+	return (
+		<div className="w-full bg-footerBottom py-8">
+			<div className="max-w-5xl mx-auto">
+				<div></div>
+			</div>
+		</div>
+	);
 };
 
 export default FooterBottom;
