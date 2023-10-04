fb39906e36005c88f86afd3fbcd292a5ef37f047
Bereket18 <bereketadamsseged@gmail.com>
2023-10-04 18:53:35 +0300

    router position done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@
 // import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header";
-import Banner from "./components/Home/Banner";
+
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Outlet,
+	Route,
+	RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+
+const Layout = () => {
+	return (
+		<div>
+			<Header />
+			<Outlet />
+			<Footer />
+		</div>
+	);
+};
 
 function App() {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />}></Route>
+			</Route>
+		)
+	);
 	return (
 		<div className="font-bodyFont">
-			<Header />
-			<Banner />
-			<Footer />
+			<RouterProvider router={router}></RouterProvider>
 		</div>
 	);
 }
